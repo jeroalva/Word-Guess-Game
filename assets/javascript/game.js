d27847d576ab0b0621349f4dba8888c7a7c81149
Jero Alvarez <jeroalva@gmail.com>
2019-09-23 08:51:03 -0500

    Agregue el array de fotos y lo necesario para que cuando el usuario gane entonces se cambie la foto (me falta ver de que manera limito el ancho/alto maximo de la foto pero si permito que sea responsiva para cuando sea pantalla chica aunque de todas formas el programa no funciona en mobile por cuestion del onkeyup, tambien agregue una cancion que se pone en play cuando gana el usuario

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,21 @@
 var guessesLeft = 15;
-var modelosTabletas = ["AluexLura", "JabeAluminio", "AluexRectangular", "AluexCircular", "AluexClasica"];
+var modelosTabletas = ["AluexLura", "JabeAluminio", "Lumex", "Loral", "Malva"];
 var inputsMade = [];
 var guessesMade = [];
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = "assets/images/Aluex-Circular-1.jpg";
+imgArray[1] = new Image();
+imgArray[1].src = "assets/images/Aluex-Clasica-64.jpg";
+imgArray[2] = new Image();
+imgArray[2].src = "assets/images/Imagen004.jpg";
+imgArray[3] = new Image();
+imgArray[3].src = "assets/images/Jabe-de-Aluminio-68.jpg";
+imgArray[4] = new Image();
+imgArray[4].src = "assets/images/Malva-17.jpg";
+
+var cancion = new Audio("assets/songs/Celebration.mp3")
 
 var imagenRes = document.getElementById("fotoRes");
 var espaciosLetras = document.getElementById("letras");
@@ -34,6 +48,8 @@ function estatusJuego(){
         }
         else if(intentosRestan>=0 && faltanLetras<=0){
             intentosRemanentes.textContent="Ganaste!";
+            imagenRes.src = imgArray[valorAleatorio].src;
+            cancion.play();
         }
 }
 
@@ -63,3 +79,25 @@ document.onkeyup = function(letraPresionada){
         faltanLetras = tabletaAleatoria.length - tabletaAleatoria.replace(/[^*]/g,"").length;
         estatusJuego();
 }
+
+var botonR = document.getElementById("botonReinicio");
+botonR.addEventListener("click", function(boton){
+intentosRemanentes.textContent = "Presiona cualquier tecla";
+valorAleatorio = Math.floor(Math.random() * modelosTabletas.length);
+tabletaAleatoria = modelosTabletas[valorAleatorio].toLowerCase();
+underScores="";
+for(var i=1; i<=tabletaAleatoria.length; i++){
+    underScores = underScores + "_ ";
+    espaciosLetras.textContent = underScores;
+}
+cuentaIntentos = 0;
+intentosRestan = tabletaAleatoria.length * 2;
+letrasIntentadas.textContent = "Presiona cualquier tecla!";
+letIntentadas = "";
+faltanLetras = tabletaAleatoria.length;
+imagenRes.src = "assets/images/ImagenInicial.jpg";
+cancion.pause();
+cancion.currentTime=0;
+console.log(tabletaAleatoria);
+console.log(botonR);
+})
